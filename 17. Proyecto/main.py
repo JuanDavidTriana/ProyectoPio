cba90587ba894c277efe1f0fae044ca87bdee5fc
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-09-02 17:57:40 -0500

    menu

--- 17. Proyecto/main.py
+++ 17. Proyecto/main.py
@@ -1,3 +1,5 @@
+import os
+
 productos = [
   {"nombre": "Anillo de diamante", "precio": 1000, "cantidad": 5},
   {"nombre": "Anillo de cuarzo", "precio": 20, "cantidad": 8},
@@ -6,12 +8,22 @@ productos = [
 ]
 carrito = []
 
+def limpiar_pantalla():
+    if os.name == 'nt':
+        os.system('cls')  # Windows
+    else:
+        os.system('clear')  # Linux o Mac
+        
+
 def mostrar_productos():
+  limpiar_pantalla()
   print(" -----------------------/Menú de productos-----------------------------------")
   for i, producto in enumerate (productos):
     print(f"{i+1}. {producto["nombre"]} - precio ${producto["precio"]} - cantidad {producto["cantidad"]}")
+  
 
 def agregar_al_carrito():
+  limpiar_pantalla()
   mostrar_productos()
   try:
     opcion = int(input("introduce el producto que desees agregar al carrito "))
@@ -28,6 +40,7 @@ def agregar_al_carrito():
     print("Se ha presentado un error", e)
 
 def mostrar_carrito():
+  limpiar_pantalla()
   if carrito:
     print("Carrito de compras.")
     for i, item in enumerate(carrito,1):
@@ -40,6 +53,7 @@ def calcular_total():
   print(f"El total de pagar es {total}")
 
 def finalizar_compra():
+  limpiar_pantalla()
   mostrar_carrito()
   if carrito:
     calcular_total()
@@ -55,30 +69,36 @@ def finalizar_compra():
 
 def main():
   while True: 
-    print(" -----------------------////-----------------------------------------")
+    limpiar_pantalla()
+    print(" -----------------------Menu Joyeria-----------------------------------------")
     print("1-Mostrar productos disponibles")
     print("2- Añadir productos al carrito")
     print("3- Mostrar carrito")
     print("4- Finalizar compra, (pagar)")
     print("5- Salir de la compra")
+    
     try:
-      selection = int(input("Introduce el número de la opción a la cuál quieres ingresar: "))
-      if selection == 1:
-        mostrar_productos()
-      elif selection == 2:
-        agregar_al_carrito()
-      elif selection == 3:
-        mostrar_carrito()
-      elif selection == 4:
-        finalizar_compra()
-      elif selection == 5:
+      opciones = {
+        1: mostrar_productos,
+        2: agregar_al_carrito,
+        3: mostrar_carrito,
+        4: finalizar_compra,
+      }
+
+      selecion = int(input("Ingrese opcion:"))
+
+      if selecion in opciones:
+        opciones[selecion]()
+        input("Enter para continuar.......")
+      elif selecion == 5:
         break
-      else:
-        print("Selección no valida, por favor seleccionar del 1 al 5")
+
     except ValueError:
       print("Entrada invalida, por favor ingrese un número")
+      input("Enter para continuar.......")
     except Exception:
       print("Se ha presentado un error")
+      input("Enter para continuar.......")
 
 
 main()
